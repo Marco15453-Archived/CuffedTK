ba930b8895b7cc2de5dd3d45a9da8ec2fccf3db2
Marco15453 <marcolpder@gmail.com>
2022-02-18 05:10:24 +0100

    Update EXILED 5.0.0
    Updating to EXILED 5.0.0
    PRE-RELEASE

--- CuffedTK/EventHandler.cs
+++ CuffedTK/EventHandler.cs
@@ -12,13 +12,13 @@ namespace CuffedTK
         {
             if (!ev.Target.IsCuffed || ev.Target == null || ev.Attacker == null || ev.Handler.Type == DamageType.Unknown) return;
 
-            if (plugin.Config.DisallowedDamageTypes.Contains(ev.Handler.Type) && (ev.Target.Team == Team.CDP || ev.Target.Team == Team.RSC))
+            if (plugin.Config.DisallowedDamageTypes.Contains(ev.Handler.Type) && (ev.Target.Role.Team == Team.CDP || ev.Target.Role.Team == Team.RSC))
             {
                 if (plugin.Config.DamageTypesTime > 0)
                     ev.Attacker.ShowHint(plugin.Config.DamageTypesMessage.Replace("%PLAYER%", ev.Target.Nickname).Replace("%DAMAGETYPE%", ev.Handler.Type.ToString()), plugin.Config.DamageTypesTime);
                 ev.IsAllowed = false;
             }
-            else if ((ev.Target.Team == Team.CDP && plugin.Config.DisallowDamagetodclass.Contains(ev.Attacker.Team)) || (ev.Target.Team == Team.RSC && plugin.Config.DisallowDamagetoScientists.Contains(ev.Attacker.Team)))
+            else if ((ev.Target.Role.Team == Team.CDP && plugin.Config.DisallowDamagetodclass.Contains(ev.Attacker.Role.Team)) || (ev.Target.Role.Team == Team.RSC && plugin.Config.DisallowDamagetoScientists.Contains(ev.Attacker.Role.Team)))
             {
                 if (plugin.Config.AttackerHintTime > 0)
                     ev.Attacker.ShowHint(plugin.Config.AttackerHint.Replace("%PLAYER%", ev.Target.Nickname), plugin.Config.AttackerHintTime);
